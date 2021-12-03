33383f120662c8847764c604d953ce65081eedc8
_RC_ <randy.white.1986@gmail.com>
2021-12-03 15:12:35 -0600

    Update sandpiles.h

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -4,5 +4,10 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 int stabilize(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+int check_grids(int grid1[3][3]);
+static void print_grid(int grid[3][3]);
+void change_grids(int grid1[3][3]);
 
 #endif
